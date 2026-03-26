0f9cb436804868c6eeb3f2391d469afc03f7473f
mikaila <mikailamohammad23@gmail.com>
2026-03-26 09:28:42 +0100

    styling

--- list.js
+++ list.js
@@ -1,8 +1,8 @@
 export let list = [
-    {name: 'Phone', price: 200, des: 'From 0% To 100%'},
-    {name: 'Battery', price: 200, des: 'From 0% To 100%'},
-    {name: 'Mp3', price: 200, des: 'From 0% To 100%'},
-    {name: 'Power-Bank', price: 300, des: 'From 0% To 100%'},
-    {name: 'Touch-light', price: 200, des: 'From 0% To 100'},
-    {name: 'Computer(Laptop)', price: 500, des: 'From 0% To 100'}
+    {id: 1, name: 'Phone', price: 200, des: 'From 0% To 100%'},
+    {id: 2, name: 'Battery', price: 200, des: 'From 0% To 100%'},
+    {id: 3, name: 'Mp3', price: 200, des: 'From 0% To 100%'},
+    {id: 4, name: 'Power-Bank', price: 300, des: 'From 0% To 100%'},
+    {id: 5, name: 'Touch-light', price: 200, des: 'From 0% To 100%'},
+    {id: 6, name: 'Computer(Laptop)', price: 500, des: 'From 0% To 100%'}
 ];
--- scripts.js
+++ scripts.js
@@ -1,22 +1,40 @@
 import { list } from "./list.js";
-console.log(list);
 
 let listHTML = '';
 
 list.forEach((item) => {
-   
-    listHTML += `<div class="content">
+  listHTML += `<div class="content">
     <div>Category: ${item.name}</div>
     <div>Price: ${item.price}</div>
     <div>Description: ${item.des}</div>
-     </div>
-    `;
-    function btn(){
-        console.log('hello')
-    }
-  
-})
+  </div>`;
+});
 
-document.querySelector('.main-container')
-.innerHTML += listHTML;
-  
+const listContainer = document.querySelector('.list-container');
+
+//  nan ne za ka daura shi a page
+listContainer.innerHTML = listHTML;
+
+
+
+document.getElementById('button').addEventListener("click", ()=>{
+
+  listHTML = ''; //  important ka reset
+
+  const updated = list.map((des) => {
+    return {...des, price: des.price - 50}
+  });
+
+  updated.forEach((ol) => {
+    listHTML += `
+     <div class="content">
+      <div>Category: ${ol.name}</div>
+      <div>Price: ${ol.price}</div>
+      <div>Description: ${ol.des}</div>
+</div>
+    `;
+  });
+
+  //  update page bayan click
+  listContainer.innerHTML = listHTML;
+});
